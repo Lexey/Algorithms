db7ec3dee1069950ba24784f78d32ff3ddd79ee9
Alexey Ostapenko <lexeyo@gmail.com>
2012-08-26 21:13:17 +0530

    initial release

---
+++ DisjointSets/DisjointSets.cs
@@ -0,0 +1,30 @@
+﻿namespace Structures
+{
+    /// <summary>Реализация классической структуры Disjoint sets</summary>
+    ///  <remarks>
+    /// http://en.wikipedia.org/wiki/Disjoint-set_data_structure
+    /// </remarks>
+    public sealed class DisjointSets : DisjointSetsBase
+    {
+        /// <summary>Создает пустой DS</summary>
+        public DisjointSets() {}
+
+        /// <summary>Создает DS с заданным количеством элементов</summary>
+        /// <param name="count">Количество элементов</param>
+        public DisjointSets(int count)
+        {
+            Add(count);
+        }
+
+        /// <summary>Добавление заданного количества элементов</summary>
+        /// <param name="count">Количество элементов</param>
+        public void Add(int count)
+        {
+            for (var i = 0; i < count; ++i)
+            {
+                Nodes.Add(new BasicNode { ParentIndex = -1, Rank = 0 });
+            }
+            SetCount += count;
+        }
+    }
+}
---
+++ DisjointSets/DisjointSetsT.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Structures
+{
+    /// <summary>
+    /// Реализация классической структуры Disjoint sets
+    /// Generic - версия, позволяющая хранить данные внутри
+    /// </summary>
+    ///  <remarks>
+    /// http://en.wikipedia.org/wiki/Disjoint-set_data_structure
+    /// </remarks>
+    public sealed class DisjointSets<T> : DisjointSetsBase
+    {
+        /// <summary>Создает пустой DS</summary>
+        public DisjointSets() {}
+
+        /// <summary>Создает DS со значениями из перечисления</summary>
+        /// <param name="values">Перечисление значений для добавления</param>
+        public DisjointSets(IEnumerable<T> values)
+        {
+            Add(values);
+        }
+
+        /// <summary>Получение значения элемента по индексу</summary>
+        /// <param name="index">Индекс элемента</param>
+        public T this[int index]
+        {
+            get { return ((Node<T>)Nodes[index]).Value; }
+        }
+
+        /// <summary>Добавление перечисления элементов</summary>
+        /// <param name="values">Элементы</param>
+        public void Add(IEnumerable<T> values)
+        {
+            var beforeCount = Nodes.Count;
+            Nodes.AddRange(values.Select(x => new Node<T> { Value = x, ParentIndex = -1, Rank = 0 } ));
+            SetCount += Nodes.Count - beforeCount;
+        }
+
+        /// <summary>Добавление одного элемента</summary>
+        /// <param name="value">Элемент</param>
+        public void Add(T value)
+        {
+            Nodes.Add(new Node<T> { Value = value, ParentIndex = -1, Rank = 0 });
+            ++SetCount;
+        }
+
+        /// <summary>Узел дерева</summary>
+        private class Node<T2> : BasicNode
+        {
+            /// <summary>Собственно полезные данные</summary>
+            public T2 Value;
+        }
+    }
+}
